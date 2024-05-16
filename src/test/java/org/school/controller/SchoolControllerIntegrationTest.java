7dc7dd16112335c4bec36fdb29ad2fbc7f0853fd
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-17 00:51:21 +0530

    refractor

--- src/test/java/org/school/controller/SchoolControllerIntegrationTest.java
+++ src/test/java/org/school/controller/SchoolControllerIntegrationTest.java
@@ -15,14 +15,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SchoolControllerTest {
+class SchoolControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
